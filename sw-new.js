f46409049cb7ebbce0f5fdcbffb3d4bf5e1939a0
loveisthefuture125811-prog <loveisthefuture125811@gmail.com>
2026-05-19 02:33:55 -0500

    Add files via upload

---
+++ sw-new.js
@@ -0,0 +1,48 @@
+// WZ Armory Service Worker - Fresh Build
+// Timestamp cache busting: every deploy gets a new cache name
+
+const TIMESTAMP = '20250519-' + Date.now();
+const CACHE = 'wz-armory-' + TIMESTAMP;
+
+const ASSETS = [
+  './',
+  './index.html',
+  './weapons.js',
+  './app.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=VT323&display=swap'
+];
+
+// Install: cache fresh assets
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate: delete ALL old caches, claim clients immediately
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// Fetch: network-first for everything, fallback to cache
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
