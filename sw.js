db8c6ca0ae6adaa16646d5f9d5fe3c23150c1f9f
loveisthefuture125811-prog <loveisthefuture125811@gmail.com>
2026-05-19 02:10:56 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'warzone-armory-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './weapons.js',
+  './app.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=VT323&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
